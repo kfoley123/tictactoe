e55093c61df82cda7d277e2f0ce541c6431c8b34
Kortney Foley <kortneyfoley@Kortneys-MacBook-Pro.local>
2021-11-04 16:04:06 -0300

    added loop to keep tthe game going until there is a winner, prints when there is a winner, cleaned up some of the code

--- tictactoe.py
+++ tictactoe.py
@@ -6,16 +6,9 @@
 #whichever player gets three in a row or diagonal wins the game 
 # if there is no winner there is a tie 
 
-
 playerChoice = ""
-playerChoice = input("Choose X or O. Player X goes first: ")
-
 
 gameBoard = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
-print(" ", "1", "2", "3")
-print("1", gameBoard[0], gameBoard[1], gameBoard[2])
-print("2", gameBoard[3], gameBoard[4], gameBoard[5])
-print("3", gameBoard[6], gameBoard[7], gameBoard[8])
 
 coordMap = {
     "1,1": 0,
@@ -29,11 +22,57 @@ coordMap = {
     "3,3": 8
 }
 
-playerOneMove = input("Choose a space (example: 1,1): ")
+def checkForWin(board):
+    if board[0] != "_" and board[0] == board[1] == board[2]:
+        return True
+    if board[3] != "_" and board[3] == board[4] == board[5]:
+        return True
+    if board[6] != "_" and board[6] == board[7] == board[8]:
+        return True 
+    if board[0] != "_" and board[0] == board[3] == board[6]:
+        return True
+    if board[1] != "_" and board[1] == board[4] == board[7]:
+        return True
+    if board[2] != "_" and board[2] == board[5] == board[8]:
+        return True
+    if board[0] != "_" and board[0] == board[1] == board[2]:
+         return True
+    if  board[2] != "_" and board[2] == board[4] == board[6]:
+         return True
+    return False
 
-gameBoard[coordMap[playerOneMove]] = "X"
+def displayGameBoard(board):  
+    print(" ", "1", "2", "3")
+    print("1", board[0], board[1], board[2])
+    print("2", board[3], board[4], board[5])
+    print("3", board[6], board[7], board[8])
 
-print(" ", "1", "2", "3")
-print("1", gameBoard[0], gameBoard[1], gameBoard[2])
-print("2", gameBoard[3], gameBoard[4], gameBoard[5])
-print("3", gameBoard[6], gameBoard[7], gameBoard[8])
+playerChoice = input("Choose X or O. Player X goes first: ")
+
+displayGameBoard(gameBoard)
+
+
+
+while checkForWin(gameBoard) == False: 
+    playerOneMove = input("Player X choose a space (example: 1,1): ")
+
+    gameBoard[coordMap[playerOneMove]] = "X"
+
+    displayGameBoard(gameBoard)
+
+    if checkForWin(gameBoard): 
+        print("Player X Wins")
+        break
+
+    playerTwoMove = input("Player O choose a space (example: 1,1): ")
+
+    gameBoard[coordMap[playerTwoMove]] = "O"
+
+    displayGameBoard(gameBoard)
+
+    if checkForWin(gameBoard):  
+        print("Player O Wins")
+        break
+
+
+       
